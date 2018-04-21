de5bde80df99e020fc49514d6e1643daae4b2b2f
FlopieUtd <florisdehaan92@hotmail.com>
2018-04-21 14:38:46 +0200

    New splash screen, new thumb for tasks

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-var navLinks = document.querySelectorAll('.splash-screen__navigation > li');
+const splashScreen = document.querySelector('.splash-screen-background');
+
+const navLinks = document.querySelectorAll('.splash-screen__navigation > li');
 navLinks.forEach(function (link) {
 	link.addEventListener('click', function (e) {
 		scrollTo(e.target.innerHTML.toLowerCase());
@@ -10,4 +12,13 @@ function scrollTo (destination) {
 	  behavior: 'smooth',
 	  block: 'start'
 	});
+}
+
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll () {
+	const doc = document.documentElement;
+	const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+	const imgPos = top / 2 + 'px';
+	splashScreen.style.transform = 'translateY(' + imgPos + ')';
 }
