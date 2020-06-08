8ae6d3ab97d41f3906608f2b6d5ebf7bbe548967
FlopieUtd <florisdehaan92@hotmail.com>
2020-06-08 20:56:11 +0200

    Fix navigation z-index

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-const splashScreen = document.querySelector('.splash-screen-background');
-const center = document.querySelector('.center');
+const splashScreen = document.querySelector(".splash-screen-background");
+const center = document.querySelector(".center");
 
-const navLinks = document.querySelectorAll('.splash-screen__navigation > li');
+const navLinks = document.querySelectorAll(".splash-screen__navigation > li");
 for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener('click', function (e) {
+  navLinks[i].addEventListener("click", function (e) {
     scrollTo(e.target.innerHTML.toLowerCase());
-  })
+  });
 }
 
-function scrollTo (destination) {
-	document.querySelector('.' + destination).scrollIntoView({ 
-	  behavior: 'smooth',
-	  block: 'start'
-	});
+function scrollTo(destination) {
+  document.querySelector("." + destination).scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 }
 
-window.addEventListener('scroll', handleScroll);
+window.addEventListener("scroll", handleScroll);
 
-function handleScroll () {
-	const doc = document.documentElement;
-	const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
-	const imgPos = top / 2;
-	splashScreen.style.transform = `translateY(${imgPos}px)`;
-	center.style.transform = `translateY(${imgPos * .45}px)`;
+function handleScroll() {
+  const doc = document.documentElement;
+  const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+  const imgPos = top / 2;
+  splashScreen.style.transform = `translateY(${imgPos}px)`;
+  center.style.transform = `translateY(${imgPos * 0.45}px)`;
 }
