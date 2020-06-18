79fcd6717e39b4950a1466557890dd1799dca0dc
FlopieUtd <florisdehaan92@hotmail.com>
2020-06-18 22:22:15 +0200

    update backgrounds

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-const splashScreen = document.querySelector(".splash-screen-background");
+const back = document.querySelector(".splash-screen-back");
+const middle = document.querySelector(".splash-screen-middle");
+const front = document.querySelector(".splash-screen-front");
 const center = document.querySelector(".center");
+
+const num = Math.floor(Math.random() * 3) + 1;
+
+back.style.background = `url(./images/background-${num}/back.jpg) no-repeat center center`;
+middle.style.background = `url(./images/background-${num}/middle.png) no-repeat center center`;
+front.style.background = `url(./images/background-${num}/front.png) no-repeat center center`;
 
 const navLinks = document.querySelectorAll(".splash-screen__navigation > li");
 for (let i = 0; i < navLinks.length; i++) {
@@ -21,6 +29,7 @@ function handleScroll() {
   const doc = document.documentElement;
   const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
   const imgPos = top / 2;
-  splashScreen.style.transform = `translateY(${imgPos}px)`;
-  center.style.transform = `translateY(${imgPos * 0.45}px)`;
+  back.style.transform = `translateY(${imgPos}px)`;
+  center.style.transform = `translateY(${imgPos * 0.5}px)`;
+  middle.style.transform = `translateY(${imgPos * 0.15}px)`;
 }
