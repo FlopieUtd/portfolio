99c611659b96deed8a8fd2057e0b5f810ca39678
FlopieUtd <florisdehaan92@hotmail.com>
2020-06-20 11:22:57 +0200

    Control background image load order

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const back = document.querySelector(".splash-screen-back");
-const middle = document.querySelector(".splash-screen-middle");
-const front = document.querySelector(".splash-screen-front");
+const back = document.querySelector(".splash-screen__back");
+const middle = document.querySelector(".splash-screen__middle");
+const front = document.querySelector(".splash-screen__front");
 const center = document.querySelector(".center");
+const white = document.querySelector(".splash-screen__white");
 
 const num = Math.floor(Math.random() * 3) + 1;
 
 back.style.background = `url(./images/background-${num}/back.jpg) center center / cover repeat`;
-middle.style.background = `url(./images/background-${num}/middle.png) center center / cover repeat`;
-front.style.background = `url(./images/background-${num}/front.png) center center / cover repeat`;
+
+const url = `./images/background-${num}/back.jpg`;
+const img = new Image();
+img.onload = () => {
+  white.style.opacity = 0;
+  middle.style.background = `url(./images/background-${num}/middle.png) center center / cover repeat`;
+  front.style.background = `url(./images/background-${num}/front.png) center center / cover repeat`;
+};
+img.src = url;
+if (img.complete) img.onload();
 
 const navLinks = document.querySelectorAll(".splash-screen__navigation > li");
 for (let i = 0; i < navLinks.length; i++) {
